edc4e2543243d8078f16505fb50e1316c1c70f54
pallas1 <pallas1@users.noreply.github.com>
2017-03-21 15:34:46 +0100

    Fix wallet backup

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -824,8 +824,8 @@ bool BackupWallet(const CWallet& wallet, const string& strDest)
                     pathDest /= wallet.strWalletFile;
 
                 try {
-#if 1
-//                    filesystem::copy_file(pathSrc, pathDest, filesystem::copy_option::overwrite_if_exists);
+#if BOOST_VERSION >= 104000
+                    filesystem::copy_file(pathSrc, pathDest, filesystem::copy_option::overwrite_if_exists);
 #else
                     filesystem::copy_file(pathSrc, pathDest);
 #endif
